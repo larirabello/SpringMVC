bb791a050bdf3aeae02b7bf554c0f9fad5ae7420
Arllen Lira <arllenlira@gft.com>
2016-04-10 23:20:03 -0300

    mysql driver

--- SpringMVC/src/net/codejava/spring/config/MvcConfiguration.java
+++ SpringMVC/src/net/codejava/spring/config/MvcConfiguration.java
@@ -37,9 +37,9 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter{
 	public DataSource getDataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/contactdb");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/starterlab");
 		dataSource.setUsername("root");
-		dataSource.setPassword("P@ssw0rd");
+		dataSource.setPassword("root");
 		
 		return dataSource;
 	}
